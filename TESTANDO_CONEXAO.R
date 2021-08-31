10b8cf4e676841cc74fc68f173f5be2606eda200
Shayane Cordeiro <shayane.cordeiro@gmail.com>
2021-08-31 12:09:18 -0300

    commit c2

--- TESTANDO_CONEXAO.R
+++ TESTANDO_CONEXAO.R
@@ -6,6 +6,7 @@ sum(dados)
 # Conexão ok
 
 # Passos realizados para conexão do R no GitHub
+# Link útil do youtube: https://www.youtube.com/watch?v=-HpIUHEdB10
 
 # 1) Criar o Repositório no GitHub e copiar o endereço.
 # 2) File -> NewProject -> Version Control -> GitHub
@@ -17,11 +18,20 @@ sum(dados)
 # 3 formas;
 
 #  1) Terminal
+##  1.1) git status
+##  1.2) git add .
+##  1.3) git status
+##  1.4) git commit -m "commit at"
+##  1.5) git push
+
+
 #  2) Guia Git que fica nas guias (Environment; History; Connections; Git; Tutorial)
 ## Salve o arquivo
 ## 2.1) Selecione o arquivo e clique em Commit
 ## 2.2) Na aba commit descreva(simplique) o que foi alterado e clique em commit -> close
 ## 2.3) Após clique em Push
+
+
 #  3) Aba Git  que fica nas abas para formatações do R
 ## Na aba Git é análogo ao realizado anteriormente 
 ## Salve o arquivo e selecione; Clique em 1) Commit e 2)Push
