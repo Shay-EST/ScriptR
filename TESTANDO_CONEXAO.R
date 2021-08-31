3b57d9b085b91260e5b6ca220fc4099fead1bd1c
Shayane Cordeiro <shayane.cordeiro@gmail.com>
2021-08-31 11:43:01 -0300

    commit c

--- TESTANDO_CONEXAO.R
+++ TESTANDO_CONEXAO.R
@@ -2,3 +2,20 @@
 
 dados<-c(1,2,3,4)
 sum(dados)
+
+# Conexão ok
+
+# Passos realizados para conexão do R no GitHub
+
+# 1) Criar o Repositório no GitHub e copiar o endereço.
+# 2) File -> NewProject -> Version Control -> GitHub
+# 3) Repository Url= Cole o endereço copiado do GitHub
+# 4) Create project as subdirectory of = Selecione o subdiretório
+# 5) Elabore os códigos no R
+
+# Formas de Enviar para o GitHub
+
+# Na Guia Git 
+
+
+
