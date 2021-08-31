3777a18752c6680879acf7b087744410ccaf4009
Shayane Cordeiro <shayane.cordeiro@gmail.com>
2021-08-31 11:49:56 -0300

    commit c1

--- TESTANDO_CONEXAO.R
+++ TESTANDO_CONEXAO.R
@@ -14,8 +14,20 @@ sum(dados)
 # 5) Elabore os códigos no R
 
 # Formas de Enviar para o GitHub
+# 3 formas;
 
-# Na Guia Git 
+#  1) Terminal
+#  2) Guia Git que fica nas guias (Environment; History; Connections; Git; Tutorial)
+## Salve o arquivo
+## 2.1) Selecione o arquivo e clique em Commit
+## 2.2) Na aba commit descreva(simplique) o que foi alterado e clique em commit -> close
+## 2.3) Após clique em Push
+#  3) Aba Git  que fica nas abas para formatações do R
+## Na aba Git é análogo ao realizado anteriormente 
+## Salve o arquivo e selecione; Clique em 1) Commit e 2)Push
+
+
+
 
 
 
